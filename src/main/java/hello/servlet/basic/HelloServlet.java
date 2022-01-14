d6368610faa4cb61d5438328f4a9ee30bbb67c04
woopilChoi <woopil.dev@gmail.com>
2022-01-14 13:52:45 +0900

    Merge branch 'master' of https://github.com/woopil/servlet

--- src/main/java/hello/servlet/basic/HelloServlet.java
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -1,2 +1,28 @@
-package hello.servlet.basic;public class HolloServlet {
+package hello.servlet.basic;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        final String username = request.getParameter("username");
+        System.out.println("username = " + username);
+        // Response Header
+        response.setContentType("text/plan");
+        response.setCharacterEncoding("utf-8");
+        // Body
+        response.getWriter().write("hello " + username);
+    }
 }
